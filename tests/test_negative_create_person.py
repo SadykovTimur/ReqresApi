2bbc37fb72e93d2df18997ac6b63c8b8e0c0d051
Timur_heart <Sady-timur@mail.ru>
2023-05-16 11:35:36 +0300

    Added test_negative_create_person.py

--- tests/test_negative_create_person.py
+++ tests/test_negative_create_person.py
@@ -2,15 +2,15 @@ from requests import Response
 from utils.api import Api_person
 import json
 from utils.checking import Checking
-
+import pytest
 
 class Test_api_person():
 
-
+    @pytest.mark.xfail(strict=True) #пометил как ожидаемо падающий тест, использовал параметр Strict со значением True для того чтобы если тест неожиданно пройдет то вызовет падение теста
     def test_create_person_negative(self):
         print('метод POST')
         result_post: Response = Api_person.create_person()
-        Checking.check_status_code(result_post, 201) #проверка статус кода
+        Checking.check_status_code(result_post, 400) #проверка статус кода
         token = json.loads(result_post.text)
         print(list(token))  # это команда нужна для того чтобы найти тело ответа запроса
         Checking.check_json_token(result_post, ['id', 'token'])
